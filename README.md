be17bee47357fc5b8d633eb9a23df77893b8ac63
LuizBedim <LuizOMBedim@gmail.com>
2022-04-26 10:36:44 -0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Agenda
+# Agenda de compromissos
+
+<h3> Agenda de compromissos em django </h3>
+
+Inicialmente, você deve executar:
+'''
+pip install django 
+'''
+Para instalar a framework necessária para que este projeto funcione.
--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -5,3 +5,4 @@ from core.models import AgendaCompromissos
 @admin.register(AgendaCompromissos)
 class AgendaCompromissosAdmin(admin.ModelAdmin):
     list_display = ('compromisso', 'data', 'hora_inicio', 'hora_termino', 'local')
+    list_filter = ('data', 'local')
